f633bcbdf3c41a420df9361c77c848dd9e4b8875
unknown <itsomar278@gmail.com>
2022-11-27 02:30:16 +0200

    User Data Transfer Object Class Added

---
+++ WebApplication1/Models/Entites/UsersDto.cs
@@ -0,0 +1,9 @@
+
+namespace WebApplication1.Models.Entites
+{
+	public class UsersDto
+	{
+		public string UserEmail { get; set; } = string.Empty;
+		public string Password { get; set; } = string.Empty;
+	}
+}
